bbad5f149c98eb8dd86c56c202252800a9f01730
admin <hieundph29367@fpt.edu.vn>
2024-01-08 22:35:14 +0700

    kt

--- Base-Project-react/src/components/layouts/LayoutWebsite.tsx
+++ Base-Project-react/src/components/layouts/LayoutWebsite.tsx
@@ -11,17 +11,6 @@ import Tab2 from "../tabcon/tab2";
 
 const LayoutWebsite = () => {
 
-  const handleInputChange = (value: string) => {
-    setConfirmationContent(value);
-  };
-
-  const [confirmationContent, setConfirmationContent] = useState('');
-
-  const handleConfirmation = () => {
-    alert(confirmationContent);
-    setConfirmationContent("");
-  }
-
   const [currentTab, setCurrentTab] = useState(1);
   const handleTabChange = (tabNumber1 : number) => {
     setCurrentTab(tabNumber1);
@@ -45,22 +34,3 @@ export default LayoutWebsite;
 
 
 
-
-     {/* <Title  title='Form Your Essay' />
-        <Input1  onInputChange={handleInputChange}/>
-       
-        <div className='text-center mt-[-20px] font-bold '>
-        <Confom  buttonContent='Generate Quizz' onClick={handleConfirmation}/>
-        </div>
-
-        <div className=" w-[680px] ml-[640px] border rounded-lg p-20 mt-5">
-          <div className='ml-[-40px] mt-[-40px]'>
-            <Anh/>
-          </div>
-           <div className='ml-[500px] mt-[-50px]'>
-           <Title  title='Excess to 20 or more tools to convert PDF to Word....' />
-           </div>
-           <div className='mt-12'>
-           <Content content='Before you know what kindness really isyou must lose things,feel the future dissolve in a momentlike salt in a weakened broth.What you held in your hand,what you counted and carefully saved...'/>
-           </div>
-        </div> */}
